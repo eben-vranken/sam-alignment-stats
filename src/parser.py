a1f686133d98b29db9ab4b047c2e4f2464c8112d
eben-vranken <vranken.eben@outlook.com>
2026-07-20 22:21:49 +0200

    feat: read sam files and parse to type-correct dictionaries

--- src/parser.py
+++ src/parser.py
@@ -1,19 +1,29 @@
 def read_sam(file_path):
-    sequence = ""
+    reads = []
+
+    sam_fields = ["QNAME", "FLAG", "RNAME", "POS", "MAPQ", "CIGAR", "RNEXT", "PNEXT", "TLEN", "SEQ", "QUAL"]
 
     try:
         with open(file_path) as fp:
             for line in fp:
-                if line[0] != "@":
-                    for word in line.split():
-                        sequence += word
-                        
+                if not line.strip() or line.startswith("@"):
+                    continue
+
+                line_cols = line.strip().split('\t')
+                read_data = dict(zip(sam_fields, line_cols[:11]))
+                
+                read_data["FLAG"] = int(read_data["FLAG"])
+                read_data["POS"] = int(read_data["POS"])
+                read_data["MAPQ"] = int(read_data["MAPQ"])
+                read_data["TLEN"] = int(read_data["TLEN"])
+
+                reads.append(read_data)
                 
     except FileNotFoundError:
         print("File path does not point to an existing file!")
         exit(1)
 
-    return sequence
+    return reads
 
 def parse_sam():
     pass
