0ac203a93756fc9e89d4e85f1967ca373879012c
eben-vranken <vranken.eben@outlook.com>
2026-07-20 22:12:40 +0200

    feat: skip headings and parse per field

--- src/parser.py
+++ src/parser.py
@@ -4,11 +4,15 @@ def read_sam(file_path):
     try:
         with open(file_path) as fp:
             for line in fp:
-                sequence += line.strip()
+                if line[0] != "@":
+                    for word in line.split():
+                        sequence += word
+                        
+                
     except FileNotFoundError:
         print("File path does not point to an existing file!")
         exit(1)
-        
+
     return sequence
 
 def parse_sam():
